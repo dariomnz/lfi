ace768bcf47ec019d5436d9423772fbb5fb91ae3
Dariomnz <dario.mnz.mnz@gmail.com>
2024-11-28 12:52:37 +0100

    Update dev build

--- dev/unito_build.sh
+++ dev/unito_build.sh
@@ -3,9 +3,9 @@ set -e
 mkdir -p build
 cd build
 
-export LD_LIBRARY_PATH=$HOME/dariomnz/bin/mpich/lib/:$LD_LIBRARY_PATH
-export PATH=$HOME/dariomnz/bin/mpich/bin/:$PATH
+export LD_LIBRARY_PATH=$HOME/dariomnz/bin/mpich-ch4/lib/:$LD_LIBRARY_PATH
+export PATH=$HOME/dariomnz/bin/mpich-ch4/bin/:$PATH
 
-cmake .. -D BUILD_EXAMPLES=1 -D LIBFABRIC_PATH=/opt/libfabric -D MPI_PATH=$HOME/dariomnz/bin/mpich
+cmake .. -D BUILD_EXAMPLES=1 -D LIBFABRIC_PATH=/opt/libfabric -D MPI_PATH=$HOME/dariomnz/bin/mpich-ch4
 
 cmake --build . -j 8
